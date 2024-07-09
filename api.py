7625d37e1a6260f6688ee0924bd41987063ee3e0
priyam-harsh <harsh.priyam@humanli.ai>
2024-07-09 17:25:14 +0530

    jdfjds

--- api.py
+++ api.py
@@ -8,5 +8,5 @@ def hello():
         "message":"Hello, World!",
 },200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     app.run(port=5111)
--- run.py
+++ run.py
@@ -1,4 +1,16 @@
-from main import app
-
+import subprocess
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    subprocess.call(
+        [
+            "gunicorn",
+            "-b",
+            "0.0.0.0:8080",
+            "api:app",  # This assumes the file is named run.py
+            "--timeout",
+            "600",
+        ], 
+        shell=False,
+    )
+
